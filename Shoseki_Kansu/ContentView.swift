a34cc06ef28a5464cca18dccfe3bbca42331898f
Tatsuki Yamada <hw19a134@oecu.jp>
2021-10-15 01:36:22 +0900

    Initial Commit

---
+++ Shoseki_Kansu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shoseki_Kansu
+//
+//  Created by hw19a134 on 2021/10/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shoseki_Kansu/Shoseki_KansuApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shoseki_KansuApp.swift
+//  Shoseki_Kansu
+//
+//  Created by hw19a134 on 2021/10/15.
+//
+
+import SwiftUI
+
+@main
+struct Shoseki_KansuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
