e3ac08be06e6643955dd79f3ceda370da3e8640b
Tatsuki-Yamada <tatsuki2000082@gmail.com>
2021-11-16 12:29:54 +0900

    インデントが消えていたためつけ直した。

--- Shoseki_Kansu/barcode_scan2.swift
+++ Shoseki_Kansu/barcode_scan2.swift
@@ -6,21 +6,26 @@
 //
 
 import UIKit
+
+
 class ViewController: UIViewController {
-override func viewDidLoad() {
-super.viewDidLoad()
-self.view.backgroundColor = .white
-// バーコードリーダーのモーダルが開くボタン
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        
+        // バーコードリーダーのモーダルが開くボタン
         let buttonSize: CGFloat = 100
-let button: UIButton = UIButton()
-        button.frame = CGRect(x: (self.view.frame.width - buttonSize) / 2, y: (self.view.frame.height - buttonSize) / 2, width: buttonSize, height: buttonSize)
-        button.backgroundColor = .red
-        button.setTitle("開く", for: UIControlState.normal)
-        button.addTarget(self, action: #selector(taped(sender:)), for: .touchUpInside)
-self.view.addSubview(button)
+        let button: UIButton = UIButton()
+            button.frame = CGRect(x: (self.view.frame.width - buttonSize) / 2, y: (self.view.frame.height - buttonSize) / 2, width: buttonSize, height: buttonSize)
+            button.backgroundColor = .red
+            button.setTitle("開く", for: UIControlState.normal)
+            button.addTarget(self, action: #selector(taped(sender:)), for: .touchUpInside)
+        self.view.addSubview(button)
     }
-// ボタンが押されたら呼ばれます
+    
+    
+    // ボタンが押されたら呼ばれます
     @objc func taped(sender: UIButton) {
-self.present(BarCodeReaderVC(), animated: true, completion: nil)
+        self.present(BarCodeReaderVC(), animated: true, completion: nil)
     }
 }
