671a302d2195b7168b3c05602162c73e266b575c
Tatsuki-Yamada <tatsuki2000082@gmail.com>
2021-11-16 13:17:20 +0900

    barcode_scanをファイルとして残した。

---
+++ Shoseki_Kansu/barcode_scan2.swift
@@ -0,0 +1,31 @@
+//
+//  barcode_scan2.swift
+//  Shoseki_Kansu
+//
+//  Created by hw19a050 on 2021/11/02.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        
+        // バーコードリーダーのモーダルが開くボタン
+        let buttonSize: CGFloat = 100
+        let button: UIButton = UIButton()
+            button.frame = CGRect(x: (self.view.frame.width - buttonSize) / 2, y: (self.view.frame.height - buttonSize) / 2, width: buttonSize, height: buttonSize)
+            button.backgroundColor = .red
+            button.setTitle("開く", for: UIControlState.normal)
+            button.addTarget(self, action: #selector(taped(sender:)), for: .touchUpInside)
+        self.view.addSubview(button)
+    }
+    
+    
+    // ボタンが押されたら呼ばれます
+    @objc func taped(sender: UIButton) {
+        self.present(BarCodeReaderVC(), animated: true, completion: nil)
+    }
+}
