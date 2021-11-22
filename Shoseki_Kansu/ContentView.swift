9905d9139218340b0becb80aa94dd3b321e9b4ec
Goto Yuta <hw19a050@oecu.jp>
2021-11-22 18:39:05 +0900

    カメラ表示の範囲を狭めました

--- Shoseki_Kansu/ContentView.swift
+++ Shoseki_Kansu/ContentView.swift
@@ -6,40 +6,40 @@
 //
 
 import SwiftUI
+import Alamofire
 
 struct ContentView: View {
-    @State var isPresentingScanner = false
+    @State var isPresentingScanner = true
     @State var scannedCode: String = "Scan a QR code to get started."
     @State var title: String = "title"
     
-    var scannerSheet : some View {
-        CodeScannerView(
-            codeTypes: [.ean13],
-            completion:{ result in
-                if case let .success(code) = result {
-                    // 成功したときの処理。code変数に読み取ったコードが入っている
-                    self.scannedCode = code
-                    self.isPresentingScanner = false
-                    
-                    let isbnObject = ISBNObject(isbn:code)
-                    isbnObject.GetTitle({ title in
-                        self.title = title
-                    })
-                }
-            }
-        )
-    }
+    
     var body: some View {
         VStack(spacing: 10){
+            CodeScannerView(
+                codeTypes: [.ean13],
+                completion:{ result in
+                    if case let .success(code) = result {
+                        // 成功したときの処理。code変数に読み取ったコードが入っている
+                        self.scannedCode = code
+                        self.isPresentingScanner = false
+                        
+                        let isbnObject = ISBNObject(isbn:code)
+                        isbnObject.GetTitle({ title in
+                            self.title = title
+                        })
+                    }
+                }
+            )
             Text(scannedCode)
             Text(title)
-            
-            Button("Scan QR Code"){
-                self.isPresentingScanner = true
-            }
-            .sheet(isPresented: $isPresentingScanner){
-                self.scannerSheet
-            }
+            Text(" ")
+            Text(" ")
+            Text(" ")
+            Text(" ")
+            Text(" ")
+            Text(" ")
+            Text(" ")
         }
     }
 }
